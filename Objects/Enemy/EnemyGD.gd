1600ad0901cdd7543493d09e11da5c9c3fb77283
Ismail Filipov <64804972+ismail146@users.noreply.github.com>
2021-07-24 01:57:23 +0300

    Düşman takip etme

---
+++ Objects/Enemy/EnemyGD.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+const WALK_SPEED = 100
+const GRAVITY = 600
+
+onready var player = get_node("/root/Main/Player")
+
+
+func _physics_process(delta):
+	
+	var playerPos = player.position.x
+	var enemyPos = position.x
+	var distance = abs(enemyPos-playerPos)
+
+	if distance < 200:
+		if player:
+			var direction = (player.position - position).normalized()
+			if not is_on_floor():
+				direction.y += GRAVITY
+			
+			move_and_slide(direction * WALK_SPEED)
