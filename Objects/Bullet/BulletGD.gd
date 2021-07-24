34afaa0b45349f6ef3815497a708ab0bc9abd554
Ismail Filipov <64804972+ismail146@users.noreply.github.com>
2021-07-25 02:43:51 +0300

    Mermi sahnesi ve kodu klasör değiştirildi

--- Objects/Bullet/BulletGD.gd
+++ Objects/Bullet/BulletGD.gd
@@ -5,13 +5,16 @@ var movement = Vector2()
 onready var mouse_pos = null
 
 func _ready():
+	# Mermiyi canlandirinca fare pozisyonunu degiskene almak icin
 	mouse_pos = get_local_mouse_position()
 	
 func _physics_process(delta):
+	# Mermiyi fare pozisyonuna gonderme
 	movement = movement.move_toward(mouse_pos,delta)
 	movement = movement.normalized() * speed
 	position = position + movement
 
+# Ekrandan cikinca silinmesi icin
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
 
