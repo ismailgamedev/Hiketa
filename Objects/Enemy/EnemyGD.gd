fd5baacc6e4b0e7e79a0c62fb9789546c90273de
Ismail Filipov <64804972+ismail146@users.noreply.github.com>
2021-07-25 03:53:58 +0300

    Düşman yapay zekası geliştirildi ve Animasyon eklendi

--- Objects/Enemy/EnemyGD.gd
+++ Objects/Enemy/EnemyGD.gd
@@ -1,21 +1,42 @@
 extends KinematicBody2D
 
+# Hız ve yer çekimi
 const WALK_SPEED = 100
-const GRAVITY = 600
+const GRAVITY = 25
 
+# Sahnedeki karakter sahnesine ulaşma
 onready var player = get_node("/root/Main/Player")
+onready var kol = get_node("Skeleton2D/YariGovde/UstGovde/Sag Kol")
+onready var anim_tree = get_node("AnimationTree")
+onready var state_machine = anim_tree["parameters/playback"]
 
+var direction = Vector2.ZERO
 
 func _physics_process(delta):
+	# Karakter ve Dusman arasindaki Mesafeyi ölçme
+	var distance = abs(position.x - player.position.x)
+	# Yer çekimi
 	
-	var playerPos = player.position.x
-	var enemyPos = position.x
-	var distance = enemyPos.distance_to(playerPos)
+	print(distance)
+	if not is_on_floor():
+		direction.y += GRAVITY
+	move_and_slide(direction * WALK_SPEED)
 
-	if distance < 150:
+	# Mesafeye göre takip etme ve durma 
+	if distance < 150 and distance > 100:
 		if player:
-			var direction = (player.position - position).normalized()
-			if not is_on_floor():
-				direction.y += GRAVITY
-			
-			move_and_slide(direction * WALK_SPEED)
+			direction.x = (player.position.x - position.x)
+			direction = direction.normalized() 
+	else:
+		direction.x = 0
+		
+		
+	print(direction)
+	if direction.x < -0.75 or direction.x > 0.75:
+		$AnimationPlayer.play("walk")
+	else:
+		$AnimationPlayer.play("idle")
+		
+	kol.look_at(player.global_position)
+
+	
