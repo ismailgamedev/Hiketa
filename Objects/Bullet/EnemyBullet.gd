9316b40ced5afad05849494f773d2e8e95e624f0
Ismail Filipov <64804972+ismail146@users.noreply.github.com>
2021-08-03 21:12:58 +0300

    Ses ayarlari

--- Objects/Bullet/EnemyBullet.gd
+++ Objects/Bullet/EnemyBullet.gd
@@ -6,7 +6,7 @@ onready var direction = Vector2.ZERO
 onready var origin = self.global_position
 
 func _ready():
-	player_Position = get_node("/root/Main/Player").get_global_position()
+	player_Position = get_node("/root/Level1/Player").get_global_position()
 	direction = (player_Position - origin).normalized()
 func _physics_process(delta):
 	# Mermiyi fare pozisyonuna gonderme
@@ -17,9 +17,6 @@ func _physics_process(delta):
 # Ekrandan cikinca silinmesi icin
 func _on_VisibilityNotifier2D_screen_exited():
 	self.queue_free()
-
-
-	
 
 
 func _on_EnemyBullet_area_entered(area):
