7549295bfa2a54e1ab5d052de499160f5eb43d5f
Ismail Filipov <64804972+ismail146@users.noreply.github.com>
2021-08-05 02:02:09 +0300

    Sahnelere dialoglar eklendi ve hatalar duzletildi

--- Scenes/Level2.gd
+++ Scenes/Level2.gd
@@ -1,3 +1,13 @@
 extends Node2D
 
+onready var _transition_rect := $Player/Interface/UI/SceneTraslition
 
+
+
+
+
+
+func _on_LeftEnd_body_entered(body):
+	if body.name == "Player":
+		_transition_rect.transition_to("res://Scenes/Level3.tscn")
+		
